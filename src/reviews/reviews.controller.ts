b332b2eeadfca828cb27932266f356f83ef61375
a7medsa22 <laslsalsa123@gmail.com>
2025-07-24 08:18:35 +0300

    update

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,9 +1,9 @@
 import { Controller,Get } from '@nestjs/common';
 import { ReviewService } from './reviews.service';
-@Controller({})
+@Controller('api/reviews')
 export class ReviewsController {
     constructor(private reviewService:ReviewService){}
-    @Get('api/reviews')
+    @Get()
     public getReviews() {
         return this.reviewService.getAll();
     }
--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ReviewsController } from './reviews.controller';
+import { ReviewService } from './reviews.service';
 
 @Module({
-    controllers:[ReviewsController]
+    controllers: [ReviewsController],
+    providers:[ReviewService]
 })
 export class ReviewsModule {}
