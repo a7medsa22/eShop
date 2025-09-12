6abf9273dbf02fa0caea313bf3c2745c9269e5fc
Ahmed Salah Fathy Sotohy <laslsalsa123@gmail.com>
2025-09-12 16:22:13 +0300

    fix(issues): helmet middleware and add Cors policy

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+ 
+  //Aplly Middleware 
+  app.use(helmet())
 
+   // Cors policy
+   app.enableCors({
+    origin:"http://localhost:3000"
+   })
+  // Aplly Swagger
   const swagger = new DocumentBuilder()
   .setTitle("E-Shop")
   .setVersion("1.0")
