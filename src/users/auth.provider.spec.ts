bf50799625dff6e7289cc378d8e6355567ab1785
Ahmed Salah Fathy Sotohy <laslsalsa123@gmail.com>
2025-12-17 12:03:46 +0200

    feat(auth): Add unit tests for login provider
    This commit introduces a comprehensive test suite for the `login` method in the `AuthProvider`.
    
    - Mocks the `bcryptjs` library to control password hashing and comparison in tests.
    - Adds test cases for successful login, incorrect password, non-existent user, and unverified accounts.
    - Groups existing registration tests into a `describe` block for better organization.

--- src/users/auth.provider.spec.ts
+++ src/users/auth.provider.spec.ts
@@ -8,18 +8,32 @@ import { MailService } from 'src/mails/mails.service';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { BadRequestException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+
+jest.mock('bcryptjs', () => ({
+    compare: jest.fn(),
+    genSalt: jest.fn(),
+    hash: jest.fn(),
+}));
 
 describe('AuthProvider', () => {
     let authProvider: AuthProvider;
     let userRepository: jest.Mocked<Repository<User>>;
     let mailService: jest.Mocked<MailService>;
     let configService: jest.Mocked<ConfigService>;
+    let jwtService: jest.Mocked<JwtService>;
+
+
 
     const registerDto = {
         username: 'test',
         email: 'test@test.com',
         password: 'test1234',
     };
+    const loginDto = {
+        email: 'test@test.com',
+        password: 'test1234',
+    }
     const mockUser = (overrides?: Partial<User>): User => ({
         id: 1,
         username: 'test',
@@ -63,14 +77,20 @@ describe('AuthProvider', () => {
                         get: jest.fn(),
                     },
                 },
-                { provide: JwtService, useValue: {} },
+                {
+                    provide: JwtService, useValue: {
+                        signAsync: jest.fn()
+                    }
+                },
             ],
+
         }).compile();
 
         authProvider = module.get<AuthProvider>(AuthProvider) as jest.Mocked<AuthProvider>;
         userRepository = module.get<Repository<User>>(getRepositoryToken(User)) as jest.Mocked<Repository<User>>;
         mailService = module.get(MailService);
         configService = module.get(ConfigService);
+        jwtService = module.get(JwtService);
     });
 
     it('should authProdvider be defined', () => {
@@ -79,48 +99,130 @@ describe('AuthProvider', () => {
     it('should userRepository be defined', () => {
         expect(userRepository).toBeDefined();
     });
-    describe('when email already exists', () => {
-        it('should throw BadRequestException', async () => {
-            const user = mockUser();
-            userRepository.findOne.mockResolvedValue(user);
+    describe('register()', () => {
+        describe('when email already exists', () => {
+            it('should throw BadRequestException', async () => {
+                const user = mockUser();
+                userRepository.findOne.mockResolvedValue(user);
 
-            await expect(authProvider.register(registerDto)).rejects.toThrow(BadRequestException);
+                await expect(authProvider.register(registerDto)).rejects.toThrow(BadRequestException);
 
-            expect(userRepository.findOne).toHaveBeenCalledWith({
-                where: { email: registerDto.email },
+                expect(userRepository.findOne).toHaveBeenCalledWith({
+                    where: { email: registerDto.email },
+                });
+
+                expect(userRepository.create).not.toHaveBeenCalled();
+                expect(userRepository.save).not.toHaveBeenCalled();
+                expect(mailService.sendVerifyEmailTempleate).not.toHaveBeenCalled();
             });
+        });
+        describe('when email does not exists', () => {
+            it('show create user and send verification email', async () => {
+                const user = mockUser({ password: 'hash-password' })
+                userRepository.findOne.mockResolvedValue(null);
+                userRepository.create.mockReturnValue(
+                    mockUser({ password: 'hashed-password' }),
+                );
+                userRepository.save.mockResolvedValue(
+                    mockUser({ id: 1 }),
+                );
 
-            expect(userRepository.create).not.toHaveBeenCalled();
-            expect(userRepository.save).not.toHaveBeenCalled();
-            expect(mailService.sendVerifyEmailTempleate).not.toHaveBeenCalled();
+                const result = await authProvider.register(registerDto);
+
+                expect(userRepository.findOne).toHaveBeenCalledWith({
+                    where: { email: registerDto.email }
+                });
+                (bcrypt.genSalt as jest.Mock).mockResolvedValue('salt');
+                (bcrypt.hash as jest.Mock).mockResolvedValue('hashed-password');
+
+                expect(userRepository.create).toHaveBeenCalled();
+                expect(userRepository.save).toHaveBeenCalled();
+
+                expect(configService.get).toHaveBeenCalled();
+                expect(mailService.sendVerifyEmailTempleate).toHaveBeenCalledTimes(1);
+
+                expect(result).toEqual({
+                    message: 'Verification token has been sent to your email'
+                });
+
+            });
         });
     });
-    describe('when email does not exists', () => {
-        it('show create user and send verification email', async () => {
-            const user = mockUser({ password: 'hash-password' })
-            userRepository.findOne.mockResolvedValue(null);
-            userRepository.create.mockReturnValue(
-                mockUser({ password: 'hashed-password' }),
-            );
-            userRepository.save.mockResolvedValue(
-                mockUser({ id: 1 }),
-            );
 
-            const result = await authProvider.register(registerDto);
+    describe('login', () => {
+        it('shuold throw if email not found', async () => {
+            userRepository.findOne.mockResolvedValue(null);
+            await expect(authProvider.login(loginDto)).rejects.toThrow(BadRequestException);
 
             expect(userRepository.findOne).toHaveBeenCalledWith({
-                where:{email:registerDto.email}
+                where: { email: loginDto.email }
             });
 
-            expect(userRepository.create).toHaveBeenCalled();
-            expect(userRepository.save).toHaveBeenCalled();
+            expect(userRepository.save).not.toHaveBeenCalled();
+            expect(mailService.sendVerifyEmailTempleate).not.toHaveBeenCalled();
 
-            expect(mailService.sendVerifyEmailTempleate).toHaveBeenCalledTimes(1);
-            
+        });
+
+        it('should throw BadRequestException if password is incorrect', async () => {
+            const user = mockUser()
+            userRepository.findOne.mockResolvedValue(user);
+
+            (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+
+
+            await expect(authProvider.login(loginDto)).rejects.toThrow(BadRequestException);
+
+            expect(userRepository.save).not.toHaveBeenCalled();
+            expect(mailService.sendVerifyEmailTempleate).not.toHaveBeenCalled();
+
+        });
+
+        it('should resend verification email if account not verified', async () => {
+            const user = mockUser({ isAccountVerified: false, verificationToken: null });
+            userRepository.findOne.mockResolvedValue(user);
+
+            (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+
+            userRepository.save.mockResolvedValue({
+                ...user,
+                verificationToken: 'verify-token',
+            });
+
+            const result = await authProvider.login(loginDto);
+
+            expect(userRepository.save).toHaveBeenCalled();
+            expect(mailService.sendVerifyEmailTempleate).toHaveBeenCalled();
+
             expect(result).toEqual({
                 message: 'Verification token has been sent to your email'
             });
-            
-        })
-    })
+        });
+        it('should return access token if login is successful', async () => {
+            const user = mockUser({ isAccountVerified: true });
+
+            userRepository.findOne.mockResolvedValue(user);
+
+            (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+
+            jwtService.signAsync.mockResolvedValue('fake-jwt-token');
+            const result = await authProvider.login(loginDto);
+
+
+            expect(jwtService.signAsync).toHaveBeenCalledWith({
+                id: user.id,
+                userType: user.userType
+            })
+
+            expect(userRepository.save).not.toHaveBeenCalled();
+            expect(mailService.sendVerifyEmailTempleate).not.toHaveBeenCalled();
+
+            expect(result).toEqual({ accessToken: 'fake-jwt-token' });
+
+
+        });
+
+
+
+
+    });
 });
